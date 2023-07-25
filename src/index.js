6dd3049b074b6fd833ccb97560e622a918b46d87
burgokmen <burgokmen@gmail.com>
2023-07-25 21:58:39 +0300

    Event Handling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,52 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
-// document.querySelector("h1").addEventListener("click",function(e){
-// alert("Bana tıkladın!")
-// });
 
+document.querySelector("h1").addEventListener("click", function (e) {
+  alert("Bana tıkladın!");
+});
 
 // Kodlar buraya gelecek!
+/* 
+--mouseover
+keydown
+wheel
+load
+focus
+resize
+scroll
+select
+dblclick
+drag / drop 
+*/
+
+// document.querySelector("footer").addEventListener("mouseover", (event) => {
+//   alert("Bana tıkladın!");
+// });
+
+document.querySelector(".footer").onmouseover = (event) => {
+  event.target.style.backgroundColor = "aliceblue";
+  event.target.style.borderTop = "2px solid #C0C0C0";
+};
+
+document.querySelector("footer").onmouseover = (event) => {
+  event.target.style.backgroundColor = "aliceblue";
+};
+
+document.querySelector("footer").onmouseout = (event) => {
+  event.target.style.backgroundColor = "";
+};
+
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach((e, i) => {
+  btns[i].ondblclick = function (event) {
+    event.target.style.textSize = "10rem";
+  };
+});
+
+btns.forEach((e, i) => {
+  btns[i].onmouseout = function (event) {
+    event.target.style.textSize = "";
+  };
+});
